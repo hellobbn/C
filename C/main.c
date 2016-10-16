5e3c7d8936bd2b67d4245d65b6a7723982500b5a
hellobbn <clfbbn@gmail.com>
2016-10-16 21:25:06 +0800

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C
+//
+//  Created by BBN on 2016/10/16.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
