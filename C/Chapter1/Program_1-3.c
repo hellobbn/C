42f14d979806d67e190c6ef71ac3ed8572d62834
hellobbn <clfbbn@gmail.com>
2016-10-17 11:00:33 +0800

    Program_1-3: Add More.

--- C/Chapter1/Program_1-3.c
+++ C/Chapter1/Program_1-3.c
@@ -35,6 +35,14 @@ int main(void)
 	printf("Variables of type double provide %u decimal digits precision. \n", DBL_DIG);
 	printf("Variables of type long double provide %u decimal digits precision. \n",
 		   LDBL_DIG);
+	printf("Variables of type char occupy %u bytes\n", sizeof(char));
+	printf("Variables of type short occupy %u bytes\n", sizeof(short));
+	printf("Variables of type int occupy %u bytes\n", sizeof(int));
+	printf("Variables of type long occupy %u bytes\n", sizeof(long));
+	printf("Variables of type long long occupy %u bytes\n", sizeof(long long));
+	printf("Variables of type float occupy %u bytes\n", sizeof(float));
+	printf("Variables of type double occupy %u bytes\n", sizeof(double));
+	printf("Variables of type long double occupy %u bytes\n", sizeof(long double));
 	return 0;
 }
 
